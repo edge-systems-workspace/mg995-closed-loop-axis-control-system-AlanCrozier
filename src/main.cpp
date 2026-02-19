f5fb4b1cbeff79e2df69f07a78838bed9992c573
alvincrozier20 <alvincrozierjhs@gmail.com>
2026-02-19 08:38:46 +0530

    define pinmode.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
+
+int pressure = 0;
 void setup() {
-// write your initialization code here
+    // write your initialization code here
+    Serial.begin(9600);
+    pinMode(A0, OUTPUT);
 }
 
-
 void loop() {
-// write your code here
+    // write your code here
+    pressure = analogRead(A0);
+    Serial.println(pressure);
+    delay(1000);
 }
